88fd13d4ce3839a377a46804054b516dc2de5830
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-14 14:09:12 -0400

    removed comment with key (whoops), fixed indentation

--- src/main.js
+++ src/main.js
@@ -1,32 +1,31 @@
-// let authKey = "a678c70e5f0d9e6d2cc1f27f9c1856157bc9ad09"; //DO NOT COMMIT. NO REALLY. DON'T DO IT.
 let promise = fetch(
   "https://api.github.com/users/addyosmani/orgs",
   {
     method: "GET",
     headers: {
-      Authorization: "token " + ""
+      Authorization: "token " + "073bfc4ab5e141035aba9ff902e9c036c504b192"
     },
   } );
 
   promise.then( function handleResponse(response) {
-  console.log(response);
-  response.json().then(function userOrgFind(userOrgs) {
-    userOrgs.forEach(function printOrgs(each) {
-      console.log(each);
-      let listItems = document.createElement("li");
-      let orgName = document.createElement("h1");
-      let avatar = document.createElement("img");
-      avatar.setAttribute("src", each.avatar_url);
-      listItems.classList.add("well");
+    console.log(response);
+    response.json().then(function userOrgFind(userOrgs) {
+      userOrgs.forEach(function printOrgs(each) {
+        console.log(each);
+        let listItems = document.createElement("li");
+        let orgName = document.createElement("h1");
+        let avatar = document.createElement("img");
+        avatar.setAttribute("src", each.avatar_url);
+        listItems.classList.add("well");
 
-      listItems.appendChild(avatar);
-      listItems.appendChild(orgName);
-      document.querySelector("#organizations ul").appendChild(listItems);
-      orgName.innerText = each.login;
-      console.log(each.login);
+        listItems.appendChild(avatar);
+        listItems.appendChild(orgName);
+        document.querySelector("#organizations ul").appendChild(listItems);
+        orgName.innerText = each.login;
+        console.log(each.login);
 
 
-    }) ;
+      }) ;
 
-  });
+    });
   });
